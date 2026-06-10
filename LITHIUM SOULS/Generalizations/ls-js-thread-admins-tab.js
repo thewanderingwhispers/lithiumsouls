373fa64d2700e64e245d2b72cefb18762c867195
foehnn. <87560864+thewanderingwhispers@users.noreply.github.com>
2026-06-10 23:35:20 +0200

    Create ls-js-thread-admins-tab.js

---
+++ LITHIUM SOULS/Generalizations/ls-js-thread-admins-tab.js
@@ -0,0 +1,24 @@
+// onglets des fiches admins
+
+document.addEventListener("DOMContentLoaded", function () {
+  const tabs = document.querySelectorAll(".lstab");
+  const contents = document.querySelectorAll(".ls-tab");
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+      const target = tab.dataset.tab;
+
+      tabs.forEach((t) => t.classList.remove("active"));
+      tab.classList.add("active");
+
+      contents.forEach((c) => c.classList.remove("active"));
+
+      const activeContent = document.querySelector(
+        `.ls-tab[data-content="${target}"]`
+      );
+      if (activeContent) {
+        activeContent.classList.add("active");
+      }
+    });
+  });
+});
